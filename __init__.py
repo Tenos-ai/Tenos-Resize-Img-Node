4c06ee2539b17d828cb1a05bb46ce483ffb78c31
Tenos.ai <info@tenos.ai>
2025-06-02 15:38:54 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .tenos_image_resize_target_pixels import TenosResizeToTargetPixels
+
+NODE_CLASS_MAPPINGS = {
+    "TenosResizeToTargetPixels": TenosResizeToTargetPixels
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Tenos-Resize-To-Target-Pixels": "Tenos Resize to ~1M Pixels"
+}
+
+print("✨ Tenos Resize node loaded! ✨")
